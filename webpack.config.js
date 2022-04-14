1ea53d5476684de55394c29eb553e69063b8d31a
Carlos Arbizu <carlos2345532@gmail.com>
2022-04-14 12:39:45 -0500

    minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,39 @@
 const path = require('path');
-const htmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        index: "./src/indexPage.js",
-        food: "./src/foodPage.js",
-        contact: "./src/contactPage.js",
+        index: "./src/index.js",
     },
-    // mode: "development",
-    // devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
-    rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader','css-loader'],
-        },
-        {
-            test: /\.(html)$/i,
-            use: ['']
-        }
-
-    ],
-    plugins: [
-
-    ],
+    mode: "production",
     performance: {
         maxEntrypointSize: 512000,
         maxAssetSize: 512000,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './dist/template.html',
+        }),
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true,
     },
-}
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader'],
+            },
+            {
+                test: /\.(woff|woff2|otf|eot|ttf|)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|svg)$/i,
+                type: 'asset/resource',
+            }
+    
+        ],
+    },
+};
